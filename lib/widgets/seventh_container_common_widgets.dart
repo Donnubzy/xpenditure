bd9189d7ee3496f57eb8d9dc01c6c6c28f989e8a
Donnubzy <donnubzy@gmail.com>
2026-01-22 14:54:19 +0100

    Mobile view set up for seventh container.

--- lib/widgets/seventh_container_common_widgets.dart
+++ lib/widgets/seventh_container_common_widgets.dart
@@ -18,133 +18,136 @@ class SeventhContainerCommonWidgets extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: AppColors.primaryWhite,
-      height: 200,
+      height: 280,
       width: double.infinity,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Icon(icon, color: iconColor, size: h!/18),
-          SizedBox(height: 10),
-          Text(boldText,
-            style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold
-            ),
-          ),
-          SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Icon(Icons.check,
-                size: 15,
-                color: AppColors.primaryGrey,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 4.0),
-                child: Text("Store Unlimited data",
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: AppColors.primaryBlack
-                  ),
-                ),
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Icon(Icons.check,
-                size: 15,
-                color: AppColors.primaryGrey,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 4.0),
-                child: Text("Export to pdf, xls, csv",
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: AppColors.primaryBlack
-                  ),
-                ),
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Icon(Icons.check,
-                size: 15,
-                color: AppColors.primaryGrey,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 4.0),
-                child: Text("Cloud server support",
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: AppColors.primaryBlack
-                  ),
-                ),
-              )
-            ],
-          ),
-          SizedBox(height: 10),
-          Text.rich(
-              TextSpan(
-                  text:  "\$$price/",
-                  style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold
-                  ),
-                  children: [
-                    TextSpan(
-                        text: "year",
-                        style: TextStyle(
-                            color: AppColors.primaryGrey,
-                            fontSize: 38
-                        )
-                    )
-                  ]
-              )
-          ),
-          Text(
-            "up to 3 user + 1.99 per user",
-            style: TextStyle(
-                fontSize: 20,
-                color: AppColors.primaryGrey
-            ),
-          ),
-          SizedBox(height: 20),
-          SizedBox(
-            height: 45,
-            child: ElevatedButton.icon(
-              onPressed: (){},
-              style: ButtonStyle(
-                  elevation: WidgetStatePropertyAll(0),
-                  backgroundColor: boldText == "Silver Plan"
-                      ? WidgetStatePropertyAll(AppColors.primaryOrange)
-                      : WidgetStatePropertyAll(AppColors.primaryWhite) ,
-                  shape: WidgetStatePropertyAll(RoundedRectangleBorder(
-                      side: BorderSide(color: AppColors.primaryOrange),
-                      borderRadius: BorderRadius.circular(10)
-                  ))
-              ),
-              label: Center(
-                child: Text("Get this",
-                  style: TextStyle(
-                      color: boldText == "Silver Plan" ?
-                      AppColors.primaryWhite : AppColors.primaryOrange
-                  ),
-                ),
-              ),
-              icon: Center(
-                child: Icon(Icons.arrow_right_alt,
-                  color: boldText == "Silver Plan" ?
-                  AppColors.primaryWhite : AppColors.primaryOrange,
-                ),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(icon, color: iconColor, size: h!/35),
+            SizedBox(height: 10),
+            Text(boldText,
+              style: TextStyle(
+                  fontSize: h!/30,
+                  fontWeight: FontWeight.bold
               ),
             ),
-          )
-        ],
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.check,
+                  size: 10,
+                  color: AppColors.primaryGrey,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 4.0),
+                  child: Text("Store Unlimited data",
+                    style: TextStyle(
+                        fontSize: 12,
+                        color: AppColors.primaryBlack
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.check,
+                  size: 10,
+                  color: AppColors.primaryGrey,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 4.0),
+                  child: Text("Export to pdf, xls, csv",
+                    style: TextStyle(
+                        fontSize: 12,
+                        color: AppColors.primaryBlack
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.check,
+                  size: 10,
+                  color: AppColors.primaryGrey,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 4.0),
+                  child: Text("Cloud server support",
+                    style: TextStyle(
+                        fontSize: 12,
+                        color: AppColors.primaryBlack
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 10),
+            Text.rich(
+                TextSpan(
+                    text:  "\$$price/",
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold
+                    ),
+                    children: [
+                      TextSpan(
+                          text: "year",
+                          style: TextStyle(
+                              color: AppColors.primaryGrey,
+                              fontSize: 14
+                          )
+                      )
+                    ]
+                )
+            ),
+            Text(
+              "up to 3 user + 1.99 per user",
+              style: TextStyle(
+                  fontSize: 12,
+                  color: AppColors.primaryGrey
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              width: 120,
+              child: ElevatedButton.icon(
+                onPressed: (){},
+                style: ButtonStyle(
+                    elevation: WidgetStatePropertyAll(0),
+                    backgroundColor: boldText == "Silver Plan"
+                        ? WidgetStatePropertyAll(AppColors.primaryOrange)
+                        : WidgetStatePropertyAll(AppColors.primaryWhite) ,
+                    shape: WidgetStatePropertyAll(RoundedRectangleBorder(
+                        side: BorderSide(color: AppColors.primaryOrange),
+                        borderRadius: BorderRadius.circular(10)
+                    ))
+                ),
+                label: Center(
+                  child: Text("Get this",
+                    style: TextStyle(
+                        color: boldText == "Silver Plan" ?
+                        AppColors.primaryWhite : AppColors.primaryOrange
+                    ),
+                  ),
+                ),
+                icon: Center(
+                  child: Icon(Icons.arrow_right_alt,
+                    color: boldText == "Silver Plan" ?
+                    AppColors.primaryWhite : AppColors.primaryOrange,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
